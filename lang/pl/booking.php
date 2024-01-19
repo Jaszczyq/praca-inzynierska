5c8820d20689b80606f2da5f65bf8aa162929389
Jakub Jaszczyk <jaszczur306@gmail.com>
2024-01-19 11:02:14 +0100

    ostylowanie bramki platnosci

---
+++ lang/pl/booking.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'payment_method' => 'Metoda płatności: ',
+    'payment_amount' => ', kwota: ',
+    'pln' => 'zł',
+    'waiting_for_payment' => 'Oczekiwanie na wpłatę',
+    'payment_error' => 'Błąd płatności',
+    'no_payment' => 'Brak płatności',
+    'pay' => 'Zapłać',
+    'name' => 'Imię',
+    'password_confirm' => 'Potwierdź hasło',
+    'role' => 'Typ konta',
+    'organizer' => 'Organizator',
+    'client' => 'Klient',
+    'password_reset' => 'Resetuj hasło',
+    'send_password_reset_link' => 'Wyslij link do resetowania hasła',
+
+
+];
--- resources/views/booking/payment_gateway.blade.php
+++ resources/views/booking/payment_gateway.blade.php
@@ -2,12 +2,22 @@
 
 @section('content')
     <div class="container mx-auto px-4">
-        <h1 class="text-2xl font-bold my-4">Metoda płatności: {{ $payment->method()->name }}, kwota: {{ $payment->amount }}</h1>
-        <div class="grid grid-cols-2 gap-4">
-            <a href="{{ route('payment.success', $payment->transaction_id) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Zapłać</a>
-            <a href="{{ route('payment.pending', $payment->transaction_id) }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Oczekiwanie na wpłatę</a>
-            <a href="{{ route('payment.fail', $payment->transaction_id) }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Błąd płatności</a>
-            <a href="{{ route('payment.no_payment', $payment->transaction_id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Brak wpłaty</a>
+        <div class="w-full max-w-xl mx-auto bg-white rounded p-5">
+            <h1 class="text-2xl font-bold mb-6 text-center">{{ __('booking.payment_method') }} {{ $payment->method()->name }}{{ __('booking.payment_amount') }} {{ $payment->amount }} {{ __('booking.pln') }}</h1>
+            <div class="grid grid-cols-2 gap-4">
+                <a href="{{ route('payment.success', $payment->transaction_id) }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline flex justify-center">
+                    {{ __('booking.pay') }}
+                </a>
+                <a href="{{ route('payment.pending', $payment->transaction_id) }}" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline flex justify-center">
+                    {{ __('booking.waiting_for_payment') }}
+                </a>
+                <a href="{{ route('payment.fail', $payment->transaction_id) }}" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline flex justify-center">
+                    {{ __('booking.payment_error') }}
+                </a>
+                <a href="{{ route('payment.no_payment', $payment->transaction_id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline flex justify-center">
+                    {{ __('booking.no_payment') }}
+                </a>
+            </div>
         </div>
     </div>
 @endsection
